6b3d246b5804325448b4e5c2bde6eae787ad5ae3
Dmitry Zakhoda <68736899+ryzerrast@users.noreply.github.com>
2021-03-27 23:34:20 +0200

    update

--- js/blog.js
+++ js/blog.js
@@ -1,7 +1,1 @@
-const filterPostsBtn = document.querySelector('.blog-filter-btn'),
-      filterPosts = document.querySelector('.blog__filter');
-
-filterPostsBtn.addEventListener('click', function() {
-   this.classList.toggle('active');
-   filterPosts.classList.toggle('active');
-});
+const filterPostsBtn=document.querySelector(".blog-filter-btn"),filterPosts=document.querySelector(".blog__filter");filterPostsBtn.addEventListener("click",function(){this.classList.toggle("active"),filterPosts.classList.toggle("active")});
